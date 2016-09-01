ed7c246a47413debee63b99f3285b16980801187
Tomas Kristiansson <tomas.kson@gmail.com>
2016-09-01 21:53:37 +0200

    Added manifest

---
+++ NuGetPackageMetadata.ps1
@@ -0,0 +1,35 @@
+#function Get-NuGetPackageMetadata {
+    [CmdLetBinding()]
+    Param(
+        [Parameter(Position = 1)]
+        [string]$Path = "."
+    )
+
+    Add-Type -AssemblyName "System.IO.Compression.FileSystem"
+    
+    $GCIParam = @{
+        'Path'    = $Path
+        'Recurse' = $true
+    }
+    
+    Get-ChildItem @GCIParam |
+    Where-Object { $_.Extension -eq '.nupkg' } |
+    ForEach-Object {
+        [IO.Compression.ZipFile]::OpenRead($_.FullName).Entries |
+        Where-Object { $_.Name -match '\.nuspec$' } |
+        ForEach-Object {
+            $memoryStream = New-Object System.IO.MemoryStream
+            $file = $_.Open()
+            $file.CopyTo($memoryStream)
+            $file.Dispose()
+            $memoryStream.Position = 0
+            $reader = New-Object System.IO.StreamReader($memoryStream)
+            [xml]$fileContent = $reader.ReadToEnd()
+            $reader.Dispose()
+            $memoryStream.Dispose()
+            $fileContent.package.metadata
+        }
+    }
+#}
+#Export-ModuleMember Get-NuGetPackageMetadata
+
