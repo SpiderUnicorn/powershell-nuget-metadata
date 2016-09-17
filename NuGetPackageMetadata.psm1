23ca1cae9b2bad3d03c6b9a87fe4bc6d24575031
Tomas Kristiansson <tomas.kson@gmail.com>
2016-09-17 22:30:50 +0200

    Small fixes

--- NuGetPackageMetadata.psm1
+++ NuGetPackageMetadata.psm1
@@ -3,29 +3,34 @@ function Get-NuGetPackageMetadata {
     [CmdLetBinding()]
     Param(
         [Parameter(Position = 1, ValueFromPipeline = $true)]
-        [string]$Path = "."
+        [string[]]$Path = "."
     )
-    
-    Add-Type -AssemblyName "System.IO.Compression.FileSystem"
-    
-    $GCIParam = @{
-        'Path'    = $Path
-        'Recurse' = $true
-    }
-    Get-ChildItem @GCIParam |
-    Where-Object { $_.Extension -eq '.nupkg' } |
-    ForEach-Object {
-        [IO.Compression.ZipFile]::OpenRead($_.FullName).Entries |
-        Where-Object { $_.Name -match '\.nuspec$' } |
-        ForEach-Object {
-            $deflateStream = $_.Open()
-            $streamReader = New-Object System.IO.StreamReader($deflateStream)
-            [System.Xml.XmlDocument]$fileContent = $streamReader.ReadToEnd()
-            $deflateStream.Dispose()
-            #$streamReader = New-Object System.IO.StreamReader($_.Open()) #shorter, but possible memory leak?
-            $streamReader.Dispose()
-            $fileContent.package.metadata
+    BEGIN {
+        Add-Type -AssemblyName "System.IO.Compression.FileSystem"
+        $GCIParam = @{
+            'Recurse' = $true
         }
+    }
+    PROCESS {
+        foreach ($p in $Path) {
+            Get-ChildItem @GCIParam -Path $p |
+            Where-Object { $_.Extension -eq '.nupkg' } |
+            ForEach-Object {
+                [IO.Compression.ZipFile]::OpenRead($_.FullName).Entries |
+                Where-Object { $_.Name -match '\.nuspec$' } |
+                ForEach-Object {
+                    $deflateStream = $_.Open()
+                    $streamReader = New-Object System.IO.StreamReader($deflateStream)
+                    [System.Xml.XmlDocument]$fileContent = $streamReader.ReadToEnd()
+                    $deflateStream.Dispose()
+                    #$streamReader = New-Object System.IO.StreamReader($_.Open()) #shorter, but possible memory leak?
+                    $streamReader.Dispose()
+                    $fileContent.package.metadata
+                }
+            }
+        }
+    }
+    END {
     }
 }
 
